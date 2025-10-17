684f01b71cedce1a0968f0ae0794f36568ca700d
Aman Srivastava <160766756+aman-coder03@users.noreply.github.com>
2025-10-17 15:32:58 +0530

    Create round.py

---
+++ app/round.py
@@ -0,0 +1,34 @@
+from .models import TaskRequest
+from .deployer import create_github_repo,notify_evaluation_url,push_files_to_github_repo,enable_github_pages
+from .llm import genereate_code_with_llm
+
+def round2():
+    print("inside round 2")
+
+
+async def  round1(request: TaskRequest):
+    try:
+        repo_response = await create_github_repo(repo_name=request.task)
+        repo_url = repo_response.get("html_url", "")
+
+        files = await genereate_code_with_llm(request)
+
+        commit_sha = await push_files_to_github_repo(repo=request.task,files=files)
+        # commit_sha = await push_files_to_github_repo(repo=request.task)
+        
+        pages_reponse = await enable_github_pages(repo=request.task)
+        pages_url = pages_reponse.get("pages_url")
+
+        # Prepare payload
+        payload = {
+            "email": request.email,
+            "task": request.task,
+            "round": request.round,
+            "nonce": request.nonce,
+            "repo_url": repo_url,
+            "commit_sha": commit_sha,
+            "pages_url": pages_url,
+        }
+        await notify_evaluation_url(request.evaluation_url, payload)
+    except Exception as e:
+        print(f"Error in background task: {e}")
